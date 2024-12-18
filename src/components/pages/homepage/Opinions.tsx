dcacef1c53853883076b77d7d5bdcde6fa454989
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-12-18 15:27:38 -0300

    bugs

--- src/components/pages/homepage/Opinions.tsx
+++ src/components/pages/homepage/Opinions.tsx
@@ -4,24 +4,6 @@ import { ratings } from "../../../utils/ratings";
 
 export const Opinions = () => {
 
-    const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-            slidesToSlide: 1
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2,
-            slidesToSlide: 1 // optional, default to 1.
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-            slidesToSlide: 1
-        }
-    }
-
     return (
         <section className="border-2 border-red-500 flex flex-row justify-center py-5">
             <div className="container flex flex-col items-center">
