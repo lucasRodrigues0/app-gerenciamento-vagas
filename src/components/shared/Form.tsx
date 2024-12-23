4f5d70b5bad21f63479b6081bfeeae752b708960
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-12-23 16:48:27 -0300

    inclusão de sombra

--- src/components/shared/Form.tsx
+++ src/components/shared/Form.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react"
 
 export const Form = ({ children }: PropsWithChildren) => {
     return (
-        <form className="flex flex-col items-center py-[50px] px-[20px] bg-slate-500 rounded-md gap-y-2">
+        <form className="flex flex-col items-center py-[50px] px-[20px] bg-slate-500 rounded-md gap-y-2 shadow-2xl">
             {
                 children
             }
