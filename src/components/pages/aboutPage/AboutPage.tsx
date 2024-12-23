828b998159e44b2af27c90eeb2ebd67b97428d12
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-12-23 15:03:39 -0300

    ajustes na página about

--- src/components/pages/aboutPage/AboutPage.tsx
+++ src/components/pages/aboutPage/AboutPage.tsx
@@ -5,22 +5,22 @@ import { DiNodejsSmall } from "react-icons/di";
 export const AboutPage = () => {
     return (
         <section className="text-white flex justify-center my-10">
-            <div className="container flex flex-col items-center">
+            <div className="container flex flex-col items-center gap-10">
                 <h1 className="text-5xl">About this project</h1>
-                <p className="text-center">This project was made by Lucas Rodrigues. The front-end was made using React with Typescript and Tailwind CSS. The back-end was made using Node.js with Express, PostgreSQL with Typeorm in the database and Json Web Token for authentication.
+                <p className="text-left">This project was made by Lucas Rodrigues. The front-end was made using React with Typescript and Tailwind CSS. The back-end was made using Node.js with Express, PostgreSQL with Typeorm in the database and Json Web Token for authentication.
                 </p>
-                <h2 className="text-3xl">See repositories below</h2>
-                <a href="https://github.com/lucasRodrigues0/app-gerenciamento-vagas" target="blank" className="text-3xl hover:text-violet-300 transition-all">
-                    <DiReact className="text-3xl" />
+                <h2 className="text-4xl">See repositories below</h2>
+                <a href="https://github.com/lucasRodrigues0/app-gerenciamento-vagas" target="blank" className="text-2xl hover:text-violet-300 transition-all">
+                    <DiReact className="inline text-2xl" />
                     Front-end Repository
                 </a>
-                <a href="https://github.com/lucasRodrigues0/api-gerenciamento-vagas" target="blank" className="text-3xl hover:text-violet-300 transition-all">
-                    <DiNodejsSmall className="text-3xl" />
+                <a href="https://github.com/lucasRodrigues0/api-gerenciamento-vagas" target="blank" className="text-2xl hover:text-violet-300 transition-all">
+                    <DiNodejsSmall className="inline text-2xl" />
                     Back-end Repository
                 </a>
-                <h2 className="text-3xl hover:text-violet-300 transition-all">
+                <h2 className="text-2xl hover:text-violet-300 transition-all">
                     <a href="https://github.com/lucasRodrigues0" target="blank">
-                        <DiGithubBadge className="text-3xl" />
+                        <DiGithubBadge className="inline text-2xl" />
                         My Github
                     </a>
                 </h2>
