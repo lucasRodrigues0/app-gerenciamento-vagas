49d93a65aab8fdb98b9b4699ac98c58883805b05
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-12-23 16:34:27 -0300

    alteração form

--- src/components/pages/signupPage/SignupPage.tsx
+++ src/components/pages/signupPage/SignupPage.tsx
@@ -6,23 +6,27 @@ export const SignupPage = () => {
         <div className="w-full h-screen border-2 border-red-500 flex justify-center items-center text-white">
             <Form>
                 <label htmlFor="name" className="w-full">Name</label>
-                <input type="text" id="name" className="w-[300px] h-[35px] rounded-md"/>
+                <input type="text" id="name" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500"/>
                 <label htmlFor="email" className="w-full">Email</label>
-                <input type="email" id="email" className="w-[300px] h-[35px] rounded-md"/>
+                <input type="email" id="email" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500"/>
                 <label htmlFor="password" className="w-full">Password</label>
-                <input type="password" id="password" className="w-[300px] h-[35px] rounded-md"/>
+                <input type="password" id="password" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500"/>
                 <label htmlFor="password-conf" className="w-full">Confirm Password</label>
-                <input type="password" id="password-conf" className="w-[300px] h-[35px] rounded-md"/>
-                <div className="flex justify-evenly w-full">
-                    <div>
-                        <label>candidate</label>
-                        <input type="radio" name="candidate" id="candidate" />
+                <input type="password" id="password-conf" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500"/>
+                {/* <div className="flex justify-evenly w-full">
+                    <div className="flex items-center">
+                        <label className="mr-1">candidate</label>
+                        <input type="radio" name="candidate" id="candidate" value="candidate" />
                     </div>
-                    <div>
-                        <label>recruiter</label>
-                        <input type="radio" name="candidate" id="recruiter" />
+                    <div className="flex items-center">
+                        <label className="mr-1">recruiter</label>
+                        <input type="radio" name="recruiter" id="recruiter" value="recruiter" />
                     </div>
-                </div>
+                </div> */}
+                <select className="text-black w-full h-6 text-center rounded-md" onChange={(e) => console.log(e.target.value)}>
+                    <option value="candidate" >Candidate</option>
+                    <option value="recruiter">Recruiter</option>
+                </select>
                 <Button>
                     Sign-up
                 </Button>
