6c27bcea2d2325c86e350d381d1c7d0c036cf739
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-12-26 14:53:21 -0300

    preparação dos forms para enviar requests

--- src/components/pages/signupPage/SignupPage.tsx
+++ src/components/pages/signupPage/SignupPage.tsx
@@ -1,20 +1,41 @@
 import { Link } from "react-router-dom";
 import { Button } from "../../shared/Button";
 import { Form } from "../../shared/Form";
+import { useState } from "react";
 
 export const SignupPage = () => {
+
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [confirm, setConfirm] = useState<string>('');
+    const [type, setType] = useState<string>('Candidate');
+
+    const info = {
+        name,
+        email,
+        password,
+        confPassword: confirm,
+        type
+    }
+
+    const submit = (e: any) => {
+        e.preventDefault();
+        console.log(info);
+    }
+
     return (
         <div className="w-full h-screen flex justify-center items-center text-white">
-            <Form>
+            <Form onSubmit={submit}>
                 <label htmlFor="name" className="w-full">Name</label>
-                <input type="text" id="name" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500"/>
+                <input type="text" id="name" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500" onChange={(e) => setName(e.target.value)}/>
                 <label htmlFor="email" className="w-full">Email</label>
-                <input type="email" id="email" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500"/>
+                <input type="email" id="email" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500" onChange={(e) => setEmail(e.target.value)}/>
                 <label htmlFor="password" className="w-full">Password</label>
-                <input type="password" id="password" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500"/>
+                <input type="password" id="password" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500" onChange={(e) => setPassword(e.target.value)}/>
                 <label htmlFor="password-conf" className="w-full">Confirm Password</label>
-                <input type="password" id="password-conf" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500"/>
-                <select className="text-black w-full h-6 text-center rounded-md" onChange={(e) => console.log(e.target.value)}>
+                <input type="password" id="password-conf" className="w-[300px] h-[35px] rounded-md text-black px-2 focus:outline-none focus:border-2 focus:border-violet-500" onChange={(e) => setConfirm(e.target.value)}/>
+                <select className="text-black w-full h-6 text-center rounded-md" onChange={(e) => setType(e.target.value)}>
                     <option value="candidate" >Candidate</option>
                     <option value="recruiter">Recruiter</option>
                 </select>
--- src/components/shared/Form.tsx
+++ src/components/shared/Form.tsx
@@ -1,11 +1,15 @@
 import { PropsWithChildren } from "react"
 
-export const Form = ({ children }: PropsWithChildren) => {
+export const Form = ({ children, onSubmit }: PropsWithChildren & props) => {
     return (
-        <form className="flex flex-col items-center py-[50px] px-[20px] bg-slate-500 rounded-md gap-y-2 shadow-2xl">
+        <form className="flex flex-col items-center py-[50px] px-[20px] bg-slate-500 rounded-md gap-y-2 shadow-2xl" onSubmit={onSubmit}>
             {
                 children
             }
         </form>
     )
+}
+
+type props = {
+    onSubmit: any;
 }
